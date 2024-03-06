45b415411cab34a86bd88a8f3c0f83e410c50859
Heloisa S. Rios <heloisasatirariosdossantos@gmail.com>
2024-03-05 21:56:01 -0300

    Adicionada à página a logo do IFMS no cabeçalho e no rodapé da página.

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,8 +1,16 @@
+import styles from "./footer.module.css";
+import Image from "next/image";
+
 export default function Footer(){
     return(
-        <footer>
-            <h5>All rights reserved</h5>
-            <ul>
+        <footer className={styles.rodape}>
+    <Image
+      width={100} 
+      height={100}
+      src={"https://www.ifms.edu.br/marcaifms.png"} />
+            <h5 class="rodape">All rights reserved</h5>
+
+            <ul className="apps">
                 <li>Instagram:</li>
                 <li>Twitter:</li>
                 <li>Contats:</li>
--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import styles from "./menu.module.css";
+import Image from "next/image";
 
 export default function Menu(){
     return(
-        <header>
-            <h3>My litlle things</h3>
+        <header className={styles.cabecalho}>
+     <Image
+      width={100} 
+      height={100}
+      src={"https://www.ifms.edu.br/marcaifms.png"} />
+      
+            <h3 class="cabecalho">My litlle things</h3>
                 <nav>
-                    <ul>
+                    <ul class="listas">
                         <Link href="/">
                            <li>Rise and shinee</li>
                         </Link>
